bca14f742480efc6242b003adfe0e6138d562c4f
Mahmood-Ahmed-Alqirshy <the-intic@outlook.com>
2024-08-12 14:57:38 +0300

    Update BaseRequest.php

--- app/Http/Requests/BaseRequest.php
+++ app/Http/Requests/BaseRequest.php
@@ -10,18 +10,17 @@ class BaseRequest extends FormRequest
 {
     protected $map = null;
 
-    protected $isPatchRequest = false;
-
     public function formattedData()
     {
         if ($this->map === null) {
             throw new Exception('unspecified $map', 3);
         }
-
         $formattedData = [];
         $validatedData = $this->validated();
         foreach ($this->map as $sourcePath => $destinationPath) {
-            if (! $this->isPatchRequest || Arr::has($validatedData, $sourcePath)) {
+            //if request method is not PATCH always true
+            //if request method is PATCH check if field exiest in validated data before mapping
+            if (! $this->isMethod('PATCH') || Arr::has($validatedData, $sourcePath)) {
                 starMapping($validatedData, $sourcePath, $formattedData, $destinationPath);
             }
         }
